c35b30a7ff3516b25a63e839a24f074abe0e0300
Fornalski <kfornalski@interia.pl>
2026-08-28 21:26:26 +0200

    ch01/fragments/05_hex_jump/C/implementation program finished

--- ch01/fragments/05_hex_dump/c/implementation.c
+++ ch01/fragments/05_hex_dump/c/implementation.c
@@ -1,38 +1,27 @@
-#include <stdint.h>
-#include<arpa/inet.h>
+#include<ctype.h>
 #include<stdio.h>
-#include <stdio.h>
-void byte_to_bits(uint8_t byte){
-    for(int i = 7; i >=0; i--){
-        printf("%d", (byte >> i) & 1);
-    }
-}
+#include<arpa/inet.h>
+#include<stdbool.h>
+
 
 int main(void) {
+uint8_t data[] = {
+    'T', 'h','e',  ' ', 'q', 'u', 'i', 'c',
+    'k', ' ', 'b', 'r', 'w', 'n', ' ',
+    'f', 'o', 'x', ' ', 'j', 'u', 'm', 'p',
+    's', ' ', 'o', 'v', 'e', 'r', '!', 0x01,
+    0x43, 0xff, 0x7f, 0x01, 'U', 'L', 'U' , 'B', 'I', 'O', 'N', 'A'
+};
+    for(size_t i = 0; i < sizeof(data); i +=16){
 
-uint32_t y = 0x12345678;
-printf("The initial value: 0x%d", y);
+        for( size_t j = 0; j < 16 && i + j < sizeof(data); j++){
+            printf("%02x ", data[i + j]);
+        }
 
-
-uint8_t *p1 = (uint8_t *)&y;
-printf("\nHost byte values for 0x12345678: \n");
-for (int i = 0; i < sizeof(y); i++){
-    printf("%02x ", p1[i]);
-}
-printf("\nNetwork byte values for 0x12345678: \n");
-
-uint32_t nx = htonl(y);
-uint8_t *p2 = (uint8_t *)&nx;
-for (int i = 0; i < sizeof(nx); i++){
-    printf("%02x ", p2[i] );
-}
-
-printf("\nInitial network value in binary:\n");
-for (int i = 0; i < sizeof(y); i++){
-    byte_to_bits(p2[i]);
-    printf(" ");    
-}
-
-
+        for( size_t j = 0; j < 16 && i + j < sizeof(data); j++){
+            if(isprint((unsigned char)data[i+j])) {printf("%c", data[i + j]);}else{printf(".");}
+        }
+        printf("\n");
+    }
 return 0;
 }
