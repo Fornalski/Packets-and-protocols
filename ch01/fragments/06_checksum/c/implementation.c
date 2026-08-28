cd663ab86a3f89c8cea2ea58993ad8a61092d1e2
Fornalski <kfornalski@interia.pl>
2026-08-28 22:52:41 +0200

    ch01 C implementation 06_checksum correct

--- ch01/fragments/06_checksum/c/implementation.c
+++ ch01/fragments/06_checksum/c/implementation.c
@@ -1,22 +1,22 @@
 
-#include<arpa/inet.h>
+#include<stdint.h>
 #include<stdio.h>
 
-uint16_t calculate_checksum(uint8_t *data, size_t len){
-    u_int32_t sum = 0;
-    for(size_t i = 0; i < len; i+=2){
-        sum += ((uint16_t) data[i] << 8) + data[i +1];
+uint16_t calculate_checksum(uint8_t *data, size_t len)
+{
+    uint32_t sum = 0;
 
-     /*   printf("%02x %02x\n", data[i], data[i+1]);
-        printf("Current sum: %08x\n", sum);*/
-    }
-    while(sum >> 16){
-        sum = (sum & 0xffff) + (sum >> 16);
-    }
-    u_int16_t checksum = ~sum;
-    return checksum;
+    for (size_t i = 0; i + 1 < len; i += 2) {
+        sum += ((uint16_t)data[i] << 8) | data[i + 1];}
+
+    if (len % 2 != 0) {
+        sum += (uint16_t)data[len - 1] << 8;}
+
+    while (sum >> 16) {
+        sum = (sum & 0xffff) + (sum >> 16);}
+
+    return (uint16_t)~sum;
 }
-
 int main(void) {
 uint8_t data[] = "The quick brown fox jumps over the lazy dog";
 size_t len = sizeof(data);
