b52e773277ddd641f3b2057440fef39410af27e1
Fornalski <kfornalski@interia.pl>
2026-09-01 01:05:22 +0200

    ch01 Python implementation 01_bytes_order

--- ch01/fragments/01_byte_order/python/implementation.py
+++ ch01/fragments/01_byte_order/python/implementation.py
@@ -4,10 +4,27 @@ Python implementation.
 
 Run: python3 implementation.py
 """
+import struct
+import numpy as np
 
 
 def main():
-    pass
+
+
+    number = np.uint32(0x00345678)
+
+    network = struct.pack("!I", number)
+    little = struct.pack("<I", number)
+
+    print("Network:", network.hex())
+    print("Little: ", little.hex())
+
+    print("Network length:", len(network))
+    print("Little length: ", len(little))
+    print("!H for uint16\n!I for uint32\n !Q for uint64\n")
+    print("! or > for Network oreder\n< for host order")
+
+
 
 
 if __name__ == "__main__":
