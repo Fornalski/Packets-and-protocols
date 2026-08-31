bacfc22f6bd839840b98b23dae81b2590cd79335
Fornalski <kfornalski@interia.pl>
2026-09-01 01:47:18 +0200

    ch01 Python implementation 02_number_bases

--- ch01/fragments/02_number_bases/python/implementation.py
+++ ch01/fragments/02_number_bases/python/implementation.py
@@ -7,8 +7,14 @@ Run: python3 implementation.py
 
 
 def main():
-    pass
+    number = 2005
+    print("Start value: ", number)
 
+    print("Binary: ", bin(number))
+    print("Hexadecimal: ", hex(number))
+    print("Decimal: ", int(number))
 
+    print("It is also possible to print like in C:\n0x",format(number,"08x"))
+    
 if __name__ == "__main__":
     main()
