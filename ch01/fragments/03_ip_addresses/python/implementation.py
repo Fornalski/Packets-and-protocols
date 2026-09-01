2cb8e1e021c297bcb06bd24ad40ced90604d7192
Fornalski <kfornalski@interia.pl>
2026-09-01 02:21:26 +0200

    ch01 Python implementation 03_ip_addresses

--- ch01/fragments/03_ip_addresses/python/implementation.py
+++ ch01/fragments/03_ip_addresses/python/implementation.py
@@ -4,11 +4,33 @@ Python implementation.
 
 Run: python3 implementation.py
 """
-
+import ipaddress 
 
 def main():
-    pass
+
+    addresses = [
+    "127.0.0.1",
+    "192.168.1.100",
+    "8.8.8.8",
+    "255.255.255.255",
+    "::1",
+    "2001:4860:4860::8888",
+    "999.999.999.999",
+    "hello"
+    ]      
+    network = ipaddress.ip_network("192.168.1.0/24")
+    for address in addresses:
+        try:
+            ip = ipaddress.ip_address(address)
+            print(f"\nIp address {address}")
+            print(f"Version: IPv{ip.version} ")
+            print(f"Is private: {ip.is_private}")
+            if( ip in network):
+                print(f"Also belongs to the network: {network}")
 
 
+        except ValueError:
+            print(f"\nInput: {address}: Invalid IP address")    
+        
 if __name__ == "__main__":
     main()
